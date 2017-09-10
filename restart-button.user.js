306d38157736ad8c82d4f47b927fef30bad0706f
morningpee <morningpee2@gmail.com>
2017-09-09 19:11:48 -0500

    "" -> ''

--- restart-button.user.js
+++ restart-button.user.js
@@ -12,32 +12,32 @@
 // @author knux
 // @run-at document-start
 // ==/UserScript==
-addEventListener("DOMContentLoaded",
+addEventListener('DOMContentLoaded',
 function(){
     function restartButton()
     {
         restartGame = function()
             {
-                var flashEl = document.getElementById("contentFlash");
+                var flashEl = document.getElementById('contentFlash');
                 flashEl.as3_tetrisGameRestart();
                 flashEl.focus();
             };
-        var restartEl = document.createElement("a");
-        restartEl.setAttribute("href", "javascript:restartGame()");
-        restartEl.innerHTML = "Restart Game";
+        var restartEl = document.createElement('a');
+        restartEl.setAttribute('href', 'javascript:restartGame()');
+        restartEl.innerHTML = 'Restart Game';
         try {
-            document.getElementById("game_options").parentNode.appendChild(document.createElement("br"));
-            document.getElementById("game_options").parentNode.appendChild(restartEl);
+            document.getElementById('game_options').parentNode.appendChild(document.createElement('br'));
+            document.getElementById('game_options').parentNode.appendChild(restartEl);
         }catch(err) {
          }
         (addFlashListener = function()
         {
             try {
-                document.getElementById("contentFlash").onkeyup = function(e){if(e.keyCode == 219) restartGame()};
+                document.getElementById('contentFlash').onkeyup = function(e){if(e.keyCode == 219) restartGame()};
             }catch(err) {
                 setTimeout(addFlashListener, 200);
             }
         })();
     }
-    restartScript = document.body.appendChild(document.createElement("script")).textContent = '(' + restartButton + ')()';
+    restartScript = document.body.appendChild(document.createElement('script')).textContent = '(' + restartButton + ')()';
 })
